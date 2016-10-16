54fdc96f2254ca23759b61ef714a1df3fb71f51d
Hoa Fu <fuhao8235@qq.com>
2016-10-16 12:52:24 +0800

    add No.386 solution

---
+++ cpp/386_LexicographicalNumbers_Cpp_1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        int current,i;
+        vector<int> result;
+        
+        current=1;
+        
+        for(i=0;i<n;++i){
+            result.push_back(current);
+            if(current*10<=n){
+                current=current*10;
+            }
+            else if(current+1<=n){
+                current++;
+                while(current%10==0){
+                    current=current/10;
+                }
+            }
+            else{
+                current=current/10+1;
+                while(current%10==0){
+                    current=current/10;
+                }
+            }
+        }
+        
+        return result;
+    }
+};
